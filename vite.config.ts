4f53cbe9d70dcb5b2a48dce8ae09f16c33e29aec
Alexander Deev <info.deev@yandex.ru>
2026-04-22 23:25:44 +0500

    init version

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/moirai/',
+  base: 'https://info-deev.github.io/moirai/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
